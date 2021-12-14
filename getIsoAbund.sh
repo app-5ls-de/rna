96b0de38d293b29dd7c2b29d569adbcbdff37b7c
P.Haussmann <p.haussmann@physik.uni-muenchen.de>
2021-12-14 13:27:56 +0100

    remove comment

--- getIsoAbund.sh
+++ getIsoAbund.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo "let isotopes = " > isoAbund.js
 curl 'https://cdn.jsdelivr.net/npm/isotope-abundances@2.0.4/ISOTOPES.json' | jq '{H,C,N,O,Na,P,K}' >> isoAbund.js
-#echo ";\n\nwindow.isoAbund = function (element) {\nif (!(element in isotopes)) return {};\n\nreturn isotopes[element];\n};" >> isoAbund.js
 
 cat << EOF >> isoAbund.js
 
