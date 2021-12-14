74e43e9274a9d200460c33dc80f6cb172a6f939c
P.Haussmann <p.haussmann@physik.uni-muenchen.de>
2021-12-14 13:27:26 +0100

    use npm link

--- getIsoAbund.sh
+++ getIsoAbund.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "let isotopes = " > isoAbund.js
-curl 'https://cdn.jsdelivr.net/gh/emptyport/isotope-abundances@6188cb99aa7a501ed23aa0d6170e0a6460b8b99f/ISOTOPES.json' | jq '{H,C,N,O,Na,P,K}' >> isoAbund.js
+curl 'https://cdn.jsdelivr.net/npm/isotope-abundances@2.0.4/ISOTOPES.json' | jq '{H,C,N,O,Na,P,K}' >> isoAbund.js
 #echo ";\n\nwindow.isoAbund = function (element) {\nif (!(element in isotopes)) return {};\n\nreturn isotopes[element];\n};" >> isoAbund.js
 
 cat << EOF >> isoAbund.js
