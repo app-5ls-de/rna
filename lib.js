51eb65add126f3215d90399638979d00b946b656
P.Haussmann <p.haussmann@physik.uni-muenchen.de>
2021-12-15 10:55:08 +0100

    reorder functions const first

--- lib.js
+++ lib.js
@@ -46,12 +46,57 @@ const cyclicMP = {
 };
 
 const reverse = (sequence) => sequence.split("").reverse().join("");
+
 const normalize_sequence = (sequence) =>
   new MolecularFormula(sequence.toUpperCase()).getLongFormula();
+
 const is_normalized_sequence = (sequence) =>
   new RegExp("^[AUGC ]+$").test(sequence);
+
 const is_sequence = (sequence) =>
   is_normalized_sequence(normalize_sequence(sequence));
+
+const get_isotopes = (formula, charge = 0, factor = 1, show_formula = false) =>
+  emass
+    .calculate(
+      new MolecularFormula(formula).subtract({ H: charge }).composition,
+      charge
+    )
+    .map(({ Mass, Abundance }) => ({ Mass, Abundance: Abundance * factor }))
+    .map((isotope) => {
+      if (show_formula) isotope.formula = new MolecularFormula(formula).formula;
+      return isotope;
+    });
+
+const get_isotopes_list = (
+  formulas,
+  charge,
+  limit = 0.000001 /* PruneLimit */
+) =>
+  formulas
+    .map((f) =>
+      get_isotopes(f.formula || f, f.charge || charge, f.factor, true)
+    )
+    .reduce((pv, cv) => [...pv, ...cv], [])
+    .sort((a, b) => b.Abundance - a.Abundance)
+    .filter(({ Abundance }) => Abundance >= limit);
+
+const sum = (array) => array.reduce((pv, cv) => pv + cv, 0);
+
+const get_components = (sequence) =>
+  new Proxy(new MolecularFormula(sequence).getComposition(), {
+    get: (target, p) => (target.hasOwnProperty(p) ? target[p] : 0),
+  });
+
+const get_component = (sequence, component) =>
+  get_components(sequence)[component];
+
+const get_length = (sequence) => sum(Object.values(get_components(sequence)));
+
+const gc_content = (sequence) =>
+  ((get_component(sequence, "G") + get_component(sequence, "C")) /
+    total_length) *
+  100;
 
 function complement(sequence, copy_invalid_chars = true) {
   let complement_pairs = [
@@ -84,45 +129,6 @@ function complement(sequence, copy_invalid_chars = true) {
 
   return sequence_complement_split.join("");
 }
-
-const get_isotopes = (formula, charge = 0, factor = 1, show_formula = false) =>
-  emass
-    .calculate(
-      new MolecularFormula(formula).subtract({ H: charge }).composition,
-      charge
-    )
-    .map(({ Mass, Abundance }) => ({ Mass, Abundance: Abundance * factor }))
-    .map((isotope) => {
-      if (show_formula) isotope.formula = new MolecularFormula(formula).formula;
-      return isotope;
-    });
-
-const get_isotopes_list = (
-  formulas,
-  charge,
-  limit = 0.000001 /* PruneLimit */
-) =>
-  formulas
-    .map((f) =>
-      get_isotopes(f.formula || f, f.charge || charge, f.factor, true)
-    )
-    .reduce((pv, cv) => [...pv, ...cv], [])
-    .sort((a, b) => b.Abundance - a.Abundance)
-    .filter(({ Abundance }) => Abundance >= limit);
-
-const sum = (array) => array.reduce((pv, cv) => pv + cv, 0);
-const get_components = (sequence) =>
-  new Proxy(new MolecularFormula(sequence).getComposition(), {
-    get: (target, p) => (target.hasOwnProperty(p) ? target[p] : 0),
-  });
-const get_component = (sequence, component) =>
-  get_components(sequence)[component];
-const get_length = (sequence) => sum(Object.values(get_components(sequence)));
-
-const gc_content = (sequence) =>
-  ((get_component(sequence, "G") + get_component(sequence, "C")) /
-    total_length) *
-  100;
 
 function melting_temperature(sequence) {
   let { A, U, G, C } = get_components(sequence);
