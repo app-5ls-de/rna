9e67a8c5e6aa1f66133ea304f7dfa0eab5ff5efd
P.Haussmann <p.haussmann@physik.uni-muenchen.de>
2021-12-23 14:11:07 +0100

    Use lib functions

--- script.js
+++ script.js
@@ -106,20 +106,14 @@ function section1() {
 
   let charge = -1 * parseInt(input_4z.value) || 0;
 
-  //let formula;
   let isotopes = get_isotopes(formula, charge);
 
-  let avgmass = formula.getMass();
-  if (charge > 0) avgmass = avgmass / charge - 1;
+  let most_abundant = isotopes.reduce((pv, cv) =>
+    pv.Abundance > cv.Abundance ? pv : cv
+  );
 
-  console.log(avgmass, get_avgmass(isotopes), get_avgmass2(formula, charge));
-
-  let abundances = isotopes.map((isotope) => isotope.Abundance);
-  let index_most_abundant = abundances.indexOf(Math.max(...abundances));
-  let mass_most_abundant = isotopes[index_most_abundant].Mass;
-
-  output_4mass.value = mass_most_abundant.toPrecision(7);
-  output_4avgmass.value = avgmass.toPrecision(7);
+  output_4mass.value = most_abundant.Mass.toPrecision(7);
+  output_4avgmass.value = get_avgmass(isotopes).toPrecision(7);
 
   let childs = [];
   for (let i = 0; i < isotopes.length; i++) {
@@ -132,121 +126,16 @@ function section1() {
         crel.td((isotope.Abundance * 100).toPrecision(3))
       ))
     );
-    if (i == index_most_abundant) row.classList.add("text-primary");
+    if (isotope.Abundance == most_abundant.Abundance)
+      row.classList.add("text-primary");
   }
   table_4body.replaceChildren(...childs);
 
   let FWHM = parseFloat(input_4fwhm.value);
-  let standardDeviation = FWHM / 2.3548;
-  let important_isotopes = isotopes.filter(
-    (isotope) => isotope.Abundance > 0.01
+  let isotopes_with_adducts = get_isotopes_list(
+    create_derivates(formula, adducts, 0.01),
+    charge
   );
-  let important_masses = important_isotopes.map((isotope) => isotope.Mass);
-
-  let isotopes_with_adducts = important_isotopes;
-  if (input_4adducts.checked) {
-    formula.add("K");
-    formula.subtract("H");
-    isotopes = emass.calculate(formula.composition, charge);
-
-    isotopes.forEach((isotope) => {
-      isotope.Abundance *= 0.08;
-    });
-    isotopes_with_adducts.push(...isotopes);
-    formula.subtract("K");
-    formula.add("H");
-
-    for (let i = 1; i < 4; i++) {
-      formula.add("Na");
-      formula.subtract("H");
-      isotopes = emass.calculate(formula.composition, charge);
-
-      isotopes.forEach((isotope) => {
-        isotope.Abundance *= Math.pow(0.4, i);
-      });
-      isotopes_with_adducts.push(...isotopes);
-    }
-  }
-
-  let important_isotopes_with_adducts = isotopes_with_adducts.filter(
-    (isotope) => isotope.Abundance > 0.01
-  );
-
-  function fn(scope) {
-    var x = scope.x;
-    let y = 0;
-
-    important_isotopes_with_adducts.forEach((isotope) => {
-      y += gaussian(x, isotope.Mass, standardDeviation) * isotope.Abundance;
-    });
-    return y * 100;
-  }
-
-  let range = [
-    Math.min(...important_masses) - 3 * standardDeviation,
-    Math.max(...important_masses) + 3 * standardDeviation,
-  ];
-  let d = (range[1] - range[0]) * 0.2;
-  range[0] -= d;
-  range[1] += d;
-
-  let contentsBounds = plot_4.getBoundingClientRect();
-  let width = 800;
-  let height = 500;
-  let ratio = contentsBounds.width / width;
-  width *= ratio;
-  height *= ratio;
-
-  let y_padding = 5;
-  let y_max = fn({ x: mass_most_abundant });
-  let options = {
-    target: "#plot_4",
-    width,
-    height,
-    grid: true,
-    yAxis: {
-      domain: [-y_padding, y_max + y_padding],
-      label: "%",
-    },
-    xAxis: {
-      label: "[u]",
-      domain: range,
-    },
-    data: [
-      {
-        graphType: "polyline",
-        fn: fn,
-      },
-    ],
-  };
-
-  let instance = functionPlot(options);
-
-  let rect = document
-    .getElementsByClassName("function-plot")[0]
-    .getElementsByClassName("zoom-and-drag")[0];
-  let old_domain = options.yAxis.domain;
-  crel(rect, {
-    on: {
-      pointerout: () => {
-        old_domain = options.yAxis.domain;
-        options.yAxis.domain = [-y_padding, y_max + y_padding];
-        functionPlot(options);
-      },
-      pointerenter: () => {
-        options.yAxis.domain = old_domain;
-        functionPlot(options);
-      },
-    },
-  });
+  plot_isotopes(isotopes_with_adducts, plot_4, FWHM);
+  return;
 }
-
-function gaussian(x, mean, standardDeviation) {
-  return Math.exp(
-    -Math.pow(x - mean, 2) / (2 * Math.pow(standardDeviation, 2))
-  );
-}
-
-setTimeout(() => {
-  if (input_1sequence.value) section1();
-}, 0);
